018623f8cc5727db41a2f5719eef58dbdb1c4e65
archi <archie.stevens51@icloud.com>
2022-12-03 23:00:09 +0000

    Day 2 Part 2

--- Day 2/main.py
+++ Day 2/main.py
@@ -8,20 +8,44 @@ score = {
   "Y": 2,
   "Z": 3
 }
+win = {
+  "A": 8,
+  "B": 9,
+  "C": 7
+}
+draw = {
+  "A": 4,
+  "B": 5,
+  "C": 6
+}
+lose = {
+  "A": 3,
+  "B": 1,
+  "C": 2
+}
 
 def main():
     importedFile = open("input.txt", 'r')
     lines = importedFile.readlines()
 
-    total = 0
+    totalPt1 = 0
+    totalPt2 = 0
 
     for line in lines:
-        total += winOrLose(line[0], line [2])
-        total += score[line[2]]
+        totalPt1 += winOrLose(line[0], line [2])
+        totalPt1 += score[line[2]]
+
+        totalPt2 += scorePart2(line[0], line [2])
 
     # Part 1 Answer
-    print(total)
+    print(totalPt1)
 
+    # Day 2 Answer
+    print(totalPt2)
+def scorePart2(player1, player2):
+    if player2 == 'X': return lose[player1]
+    elif player2 == 'Y': return draw[player1]
+    elif player2 == 'Z': return win[player1]
 
 def winOrLose(player1, player2):
     if player1 == 'A' and player2 == 'Y':         # Rock vs Paper
