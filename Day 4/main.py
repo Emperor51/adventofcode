51c5ea87088ebc89297f4cebba04ce6f13d45fa7
archi <archie.stevens51@icloud.com>
2022-12-04 11:45:22 +0000

    Day 4 Part 2

--- Day 4/main.py
+++ Day 4/main.py
@@ -7,12 +7,19 @@ def main():
     importedFile = open("input.txt", 'r')
     lines = importedFile.readlines()
     sum = 0
+
+    sum2=0
     for line in lines:
         split = line.split(',')
         aStart, aFinish, bStart, bFinish = int(line.split(',')[0].split('-')[0]), int(line.split(',')[0].split('-')[1]), int(line.split(',')[1].split('-')[0]), int(line.split(',')[1].split('-')[1])
         if (aStart >= bStart and aFinish <= bFinish) or (aStart <= bStart and aFinish >= bFinish):
-            sum +=1
-    print(sum)
+            sum += 1
+        if (aStart >= bStart and aStart <= bFinish) or (aFinish >= bStart and aFinish <= bFinish) \
+                or (bStart >= aStart and bStart <= aFinish) or (bFinish >= aStart and bFinish <= aFinish):
+            sum2 += 1
+
+    print("Total Contained:", sum)
+    print("Total Overlap:", sum2)
 
 
 
